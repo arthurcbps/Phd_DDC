b906373cca2268122342043ae2c31029010eff37
rmirandam <r.e.mirandamontero@gmail.com>
2022-09-23 19:45:49 -0400

    Vectorized code that would work if we had a server

--- pset2/pset2_q2.m
+++ pset2/pset2_q2.m
@@ -31,29 +31,26 @@ ParamsLL_linear = @(Params) x_LL(X_current, X_lag, Y_lag,Params(1),Params(2:6));
 OptimOptions = optimoptions(@fminunc,'Display','Iter','StepTolerance',10^-10,'OptimalityTolerance',10^-10);
 optimizers=fminunc(ParamsLL_linear, ones(6,1)',OptimOptions);
 
-alpha = optimizers(2:6);
+Alpha = optimizers(2:6);
 sigma = optimizers(1);
 
-% ML estimation
-clearvars -except alpha sigma 
+% ML estimation of transition parameters
+clearvars -except Alpha sigma 
 load('dataassign22.mat')
 
-%V
+%Normal draws
+Eps=normrnd(0,sigma,1,1);
+
+%ML estimation of the model
 tic
-x = V(1, ... %time
-    3*ones(size(X1,1),1), ... %states
-    X1, ... %X invariant
-    X1t(:,10), ... %X variant
-    zeros(5,1), ... %Gamma 1
-    zeros(5,1), ... %Gamma 2
-    ones(5,1), ... %Delta
-    0, ... %Trans cost
-    alpha, ... %Trans X parameters 
-    sigma, ...
-    .95) ;%Discount rate
+%Optimization
+Params_LL= @(Params) LL(LY1, Y, X1,X1t, Params(1:4),Params(5:8), Params(9:12),Params(13),Alpha, sigma, Params(14),Eps);
+%Params_LL(0*ones(14,1))
+
+OptimOptions = optimoptions(@fminunc,'Display','Iter','StepTolerance',10^-6,'OptimalityTolerance',10^-6);
+S=fminunc(Params_LL,0*ones(14,1),OptimOptions);
 toc
-%%
-% Transition LL
+%% Transition LL
 function LL = x_LL(X, Xlag, Ylag, sigma, alpha)
 N = length(X);
 W = [ones(N, 1) Xlag Ylag];
@@ -62,24 +59,71 @@ end
 
 %% Conditional valuation function
 %time/state/X1/X1t/gamma1/gamma2/delta/c/Alpha
-function Vvalue=V(t,s,X1,X1t,gamma1,gamma2, delta,c,Alpha, sigma, beta)
-    u=zeros(size(X1));
-    X1taux=Alpha(1)*ones(size(X1t,1),1)+Alpha(2)*X1t+normrnd(0,sigma,size(X1,1),1);
-    if t==9
-        for j=1:5
-            %Update X according to j and a random shock
-            X1tUpdt=X1taux+(j >= 2 && j <= 4).*(Alpha(j)*ones(size(X1t)));
-            %Ut+1
-            u(:, j)= X1tUpdt.*delta(j)+X1(:,1).*gamma1(j)+ X1(:,2).*gamma2(j)-c.*(s~=j);
-        end
-        Vvalue=log(sum(exp(u),2))+0.57721;
+function Vvalue=V(t,s,X1,X1t,gamma1,gamma2, delta,c,Alpha, sigma, beta,Eps)
+    
+    %Get sizes
+    n=size(X1t,1);
+    draws=length(Eps);
+    Vvalue=zeros(n,5);
+
+
+    %Get immediate utilities
+    u=zeros(n,5);    
+    u(:,2:5)=repmat(X1t,1,4).*repmat(delta',n,1)+repmat(X1(:,1),1,4).*repmat(gamma1',n,1)+repmat(X1(:,2),1,4).*repmat(gamma2',n,1)-c*(repmat(1:4,n,1)~=s);
+      
+    if t==10
+        
+        Vvalue=u;
+    
     else
-        for j=1:5
-            %Update X according to j and a random shock
-            X1tUpdt=X1taux+(j >= 2 && j <= 4).*(Alpha(j)*ones(size(X1t)));
-            %Vt+1
-            u(:, j)= X1tUpdt.*delta(j)+ X1(:,1).*gamma1(j)+ X1(:,2).*gamma2(j)-c.*(s~=j)+beta*V(t+1,j,X1,X1tUpdt,gamma1,gamma2, delta,c,Alpha, sigma, beta);
+
+        %Get immediate utilities
+        u=zeros(n,5);
+        u(:,2:5)=repmat(X1t,1,4).*repmat(delta',n,1)+repmat(X1(:,1),1,4).*repmat(gamma1',n,1)+repmat(X1(:,2),1,4).*repmat(gamma2',n,1)-c*(repmat(1:4,n,1)~=s);
+        
+        %Matrix to store results over simulations
+        v=zeros(n*draws,5);
+
+        %Update X according to j 
+        X1taux=Alpha(1)*ones(n,1)+Alpha(2)*X1t;
+        X1tUpdt=repmat(X1taux(:,1),1,4)+repmat([0,Alpha(3:5)],n,1);
+        
+        %Expand over simulated normal errors
+        X1tUpdt=repelem(X1tUpdt,draws,1)+repelem(Eps,n,1);
+ 
+        for j=2:5
+            
+            %Calculate and add expected optimal behavior
+            v(:,j)=beta*log(sum(exp(V(t+1,j-1,repelem(X1,draws,1),X1tUpdt(:,j-1),gamma1,gamma2, delta,c,Alpha, sigma, beta,Eps)),2))+0.57721;
+            
+            %Average over simulated normal errors
+            Vvalue(:,j)=u(:,j)+sum(reshape(v(:,j),draws,[]),1)';
+
         end
-        Vvalue=log(sum(exp(u),2))+0.57721;
+
     end
 end
+
+
+%% Log likelihood
+function LL=LL(LY1, Y, X1,X1t, gamma1,gamma2, delta,c,Alpha, sigma, beta,Eps)
+ 
+   %At time 1 calculate conditional valuations
+   Y=[LY1,Y];
+   LL=0;
+
+   for t=1:9
+       
+       CV=V(t,Y(Y(:,t)~=0,t),X1(Y(:,t)~=0,:),X1t(Y(:,t)~=0,t),gamma1,gamma2, delta,c,Alpha, sigma, beta,Eps);
+
+       n=size(Y(Y(:,t)~=0,t),1);
+
+       num=sum(CV*(repmat((1:5)',1,n)==Y(Y(:,t)~=0,t+1)'),2);
+       den=sum(CV,2);
+      
+       LL=LL-sum(log(num)-log(den));
+
+   end
+
+end
+
